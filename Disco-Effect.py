66ba992c06564bfcfcb2be50fb4a07feafb5196b
Ramy <ramy@Kurama>
2023-02-06 00:43:49 -0500

    Created Disco Effect - still need to refactor

--- Disco-Effect.py
+++ Disco-Effect.py
@@ -1,9 +1,6 @@
 from Face_Detector import FaceDetectorHaar as fdh
-import Drawing_Functions as df
 import cv2 as cv
 import numpy as np
-import itertools as it
-
 
 def initInputSource(source_num):
     return cv.VideoCapture(source_num)
@@ -14,6 +11,18 @@ def closeWindows(capture: cv.VideoCapture):
     cv.destroyAllWindows()
 
 
+def calc_frame_square(frame: np.array, sqr_dim: tuple):
+
+    (ih, iw, ic) = frame.shape
+
+    nh = ih//sqr_dim[0]
+    nw = iw//sqr_dim[1]
+
+    dimensions = (nw, nh)
+
+    return dimensions
+
+
 if __name__ == "__main__":
 
     path_to_face_cascade = f"{cv.data.haarcascades}haarcascade_frontalface_default.xml"
@@ -21,50 +30,59 @@ if __name__ == "__main__":
     capture = initInputSource(0)
     face_detector = fdh(path_to_face_cascade)
 
-   
+    frame_list = list()
 
     while True:
 
         ret, frame = capture.read()
         face_detector.setFrame(frame)
 
+        (ih, iw, ic) = frame.shape
+        new_frame = np.zeros((ih, iw, ic), np.uint8)
+        dimensions = calc_frame_square(new_frame, (4, 4))
+
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         gray_frame = cv.equalizeHist(gray_frame)
-        
 
         if not ret:
             print("Error reading frame - check video source")
             pass
 
-        (ih, iw, ic) = frame.shape
         faces = face_detector.detect_faces()
-        new_frame = np.zeros((ih, iw, ic), np.uint8)
 
         for face in faces:
 
             (x, y, w, h, face_frame) = face_detector.get_face_frame(face)
-            face_ROI = face_detector.get_face_ROI((x, y, w, h, face_frame), frame)
 
-            y2, x2, c  =0, 0, 0 
+            face_ROI = face_detector.get_face_ROI(
+                (x, y, w, h, face_frame), frame)
 
-            while (y2 + h) < ih:
+            resized = cv.resize(face_ROI, dimensions,
+                                interpolation=cv.INTER_AREA)
 
-                new_frame[y2:y2+h, x2:x2+w, c]  = face_ROI[:,: , c]
+            h = dimensions[1]
+            w = dimensions[0]
+
+            y2, x2, c = 0, 0, 0
+
+            while (y2 + h) <= ih:
+
+                new_frame[y2:y2+h, x2:x2+w, c] = resized[:, :, c]
                 x2 = x2 + w
-                
+
                 c = c + 1
 
                 if c == 3:
                     c = 0
-                
-                if(x2 + w >= iw):
+
+                if (x2 + w > iw):
                     x2 = 0
                     y2 = y2 + h
-                
-            
-        # cv.imshow('Capturing -old frfame', frame)
-        cv.imshow('Capturing -new frame', new_frame)
 
+        if len(faces) == 0:
+            cv.imshow('Capturing -new frame', frame)
+        else:
+            cv.imshow('Capturing -new frame', new_frame)
 
         if (cv.waitKey(1) == ord('q')) or (cv.waitKey(1) == ord('Q')):
             break
